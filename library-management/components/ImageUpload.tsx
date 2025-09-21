f5bb031635bc712086e3574ae9852e74c6821b03
AshimChoudhary <panghalashim7832@gmail.com>
2025-09-21 22:30:06 +0530

    ImageKit Uploads

--- library-management/components/ImageUpload.tsx
+++ library-management/components/ImageUpload.tsx
@@ -1,4 +1,97 @@
-const ImageUpload = () => {
-  return <div>ImageUpload</div>;
+'use client';
+
+import { IKImage, ImageKitProvider, IKUpload } from 'imagekitio-next';
+import config from '@/lib/config';
+import { useRef, useState } from 'react';
+import Image from 'next/image';
+
+const {
+  env: {
+    imageKit: { publicKey, urlEndpoint },
+  },
+} = config;
+
+const authenticator = async () => {
+  try {
+    const response = await fetch(`${config.env.apiEndpoint}/api/auth/imagekit`);
+    if (!response.ok) {
+      const errorText = await response.text();
+
+      throw new Error(
+        `Request failed with status ${response.status} : ${errorText}`
+      );
+    }
+
+    const data = await response.json();
+    const { signature, expire, token } = data;
+    return { token, expire, signature };
+  } catch (error: any) {
+    throw new Error(`Authentication Request Failed!: ${error.message}`);
+  }
+};
+
+const ImageUpload = ({
+  onFileChange,
+}: {
+  onFileChange: (filePath: string) => void;
+}) => {
+  const ikUploadRef = useRef(null);
+  const [file, setFile] = useState<{ filePath: string } | null>(null);
+
+  const onError = (error: any) => {
+    console.log(error);
+  };
+  const onSucess = (res: any) => {
+    setFile(res);
+    onFileChange(res.filePath);
+  };
+  return (
+    <ImageKitProvider
+      publicKey={publicKey}
+      urlEndpoint={urlEndpoint}
+      authenticator={authenticator}
+    >
+      <IKUpload
+        className="hidden"
+        ref={ikUploadRef}
+        onError={onError}
+        onSuccess={onSucess}
+        fileName="test-upload.png"
+      />
+
+      <button
+        className="upload-btn"
+        onClick={(e) => {
+          e.preventDefault();
+
+          if (ikUploadRef.current) {
+            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+            // @ts-expect-error
+            ikUploadRef.current?.click();
+          }
+        }}
+      >
+        <Image
+          src="/icons/upload.svg"
+          alt="upload-icon"
+          width={20}
+          height={20}
+          className="object-contain"
+        />
+        <p className="text-base text-light-100">Upload a File</p>
+
+        {file && <p className="upload-filename">{file.filePath}</p>}
+      </button>
+
+      {file && (
+        <IKImage
+          alt={file.filePath}
+          path={file.filePath}
+          width={500}
+          height={500}
+        />
+      )}
+    </ImageKitProvider>
+  );
 };
 export default ImageUpload;
