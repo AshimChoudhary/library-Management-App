77916ffa175a97f3486228214124e88395ab5200
AshimChoudhary <panghalashim7832@gmail.com>
2025-10-02 18:55:38 +0530

    Basic Setup of Admin Pages like Sidebar and Header Component Did

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -2,6 +2,7 @@ import { auth } from '@/auth';
 import { redirect } from 'next/navigation';
 import '@/styles/admin.css';
 import Sidebar from '@/components/admin/Sidebar';
+import Header from '@/components/admin/Header';
 
 const layout = async ({ children }: { children: React.ReactNode }) => {
   const session = await auth();
@@ -12,7 +13,7 @@ const layout = async ({ children }: { children: React.ReactNode }) => {
       <Sidebar session={session} />
 
       <div className="admin-container">
-        <p>Header</p>
+        <Header session={session} />
         {children}
       </div>
     </main>
--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,4 +1,4 @@
 const page = () => {
-  return <div>page</div>;
+  return <div>Admin Dashboard</div>;
 };
 export default page;
