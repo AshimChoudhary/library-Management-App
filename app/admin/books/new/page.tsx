39b12b393f4718dd0c3a8578bc871bde98e44440
AshimChoudhary <panghalashim7832@gmail.com>
2025-10-06 16:29:24 +0530

    Book Schema to Add Books

---
+++ app/admin/books/new/page.tsx
@@ -0,0 +1,17 @@
+import { Button } from '@/components/ui/button';
+import Link from 'next/link';
+
+const page = () => {
+  return (
+    <>
+      <Button asChild className="back-btn">
+        <Link href="/admin/books">Go Back</Link>
+      </Button>
+
+      <section className="w-full max-w-2xl">
+        <p>Book Form</p>
+      </section>
+    </>
+  );
+};
+export default page;
